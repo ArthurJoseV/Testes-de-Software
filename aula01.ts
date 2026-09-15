846b893a2a3eac41a9d0afdb49a432928eb0b9f5
ArthurJoseV <arthurjosevinicius@hotmail.com>
2026-09-15 09:41:40 -0300

    Conclusão

--- aula01.ts
+++ aula01.ts
@@ -1,16 +1,20 @@
 //Atividade 1
 
-// let pelopidasriodoce: string = "Av. Brasil";
-// pelopidasriodoce = "Nelson"
+/*
 
-//if (pelopidasriodoce == "Av. Brasil") {
-  //  console.log("Chegaremos cedo")
-//} else {
-  //  console.log("Andaremos muito")
-//}
+let pelopidasriodoce: string = "Av. Brasil";
+ pelopidasriodoce = "Nelson"
 
+if (pelopidasriodoce == "Av. Brasil") {
+   console.log("Chegaremos cedo")
+} else {
+   console.log("Andaremos muito")
+}
 
+/** */
 //Atividade 2
+
+
 
 type Nara = {
     pos: string;
@@ -19,7 +23,7 @@ type Nara = {
     fap: string;
     aws: string;
 }
-const perfil:Nara = {
+const perfil: Nara = {
     pos: "Engenharia de Qualidade de Software",
     temPaciencia: false,
     facul: "IFPE",
@@ -27,11 +31,24 @@ const perfil:Nara = {
     aws: "Em conclusão"
 }
 
-function descreverPerfil (perfil: Nara){
-    return `Ela faz pós em ${perfil.pos}, Ela tem paciência? ${perfil.temPaciencia}, Faz faculdade no ${perfil.facul}, Acha o FAP ${perfil.fap}, Está no curso da AWS e ${perfil.aws}`
+function descreverPerfil(perfil: Nara) {
+    return `Ela faz pós em ${perfil.pos};\n Ela tem paciência? ${perfil.temPaciencia};\n Faz faculdade no ${perfil.facul};\n Acha o FAP ${perfil.fap};\n Está ${perfil.aws} no curso AWS`
 }
 console.log(descreverPerfil(perfil))
 
+
+
 // atividade 3
 
+/*
+let valor1: number = 7
+let valor2: number = 0
 
+while (valor2 <= 500) {
+    console.log(`${valor1} x ${valor2} = ${valor1 * valor2}`)
+    valor2++
+
+}
+
+// atividade 4
+/** */
